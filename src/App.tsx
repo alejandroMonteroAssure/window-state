a7c850e7210b610528b449adbeee7024e11f9c32
Alejandro Montero <alejandro.montero@ucb.edu.bo>
2025-09-18 01:01:40 -0400

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+
+interface WindowSize{
+  width: number;
+  height: number;
+}
+
+const getWindowSize = (): WindowSize => ({
+  width: window.innerWidth, height: window.innerHeight
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [size, setSize] = useState<WindowSize>(() => getWindowSize());
+
+  useEffect(() => {
+    const onResize = () => setSize(getWindowSize());
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main">
+        <p> Width: {size.width}</p>
+        <p> Height: {size.height}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
